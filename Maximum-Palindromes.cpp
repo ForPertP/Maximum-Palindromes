6593c9c64cb86e06d00d8d14e4fb0ec06b382206
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-11-27 12:39:46 +0900

    Update Maximum-Palindromes.cpp

--- Maximum-Palindromes.cpp
+++ Maximum-Palindromes.cpp
@@ -22,6 +22,7 @@ long long modpow(long long a, long long e) {
     }
     return r;
 }
+
 long long modinv(long long a) {
     return modpow(a, MOD - 2);
 }
@@ -52,9 +53,31 @@ void initialize(string s) {
     }
 }
 
+/* answerQuery */
+int answerQuery(int l, int r) {
+    int freq[26];
 
-int main()
-{
+    for (int c = 0; c < 26; c++)
+        freq[c] = prefixCnt[c][r] - prefixCnt[c][l - 1];
+
+    long long left_pairs = 0;
+    long long odd        = 0;
+    long long denom      = 1;
+
+    for (int c = 0; c < 26; c++) {
+        left_pairs += freq[c] / 2;
+        if (freq[c] % 2 == 1) odd++;
+        denom = denom * fact[freq[c] / 2] % MOD;
+    }
+
+    long long ways = fact[left_pairs] * modinv(denom) % MOD;
+    if (odd > 0) ways = (ways * odd) % MOD;
+
+    return (int)ways;
+}
+
+
+int main() {
     ofstream fout(getenv("OUTPUT_PATH"));
 
     string s;
@@ -67,58 +90,53 @@ int main()
 
     int q = stoi(ltrim(rtrim(q_temp)));
 
-    for (int q_itr = 0; q_itr < q; q_itr++)
-    {
+    for (int q_itr = 0; q_itr < q; q_itr++) {
         string first_multiple_input_temp;
         getline(cin, first_multiple_input_temp);
 
-        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+        vector<string> first_multiple_input =
+            split(rtrim(first_multiple_input_temp));
 
         int l = stoi(first_multiple_input[0]);
         int r = stoi(first_multiple_input[1]);
 
         int result = answerQuery(l, r);
+
         fout << result << "\n";
     }
 
     fout.close();
-
     return 0;
 }
 
-
-
-string ltrim(const string &str)
-{
+string ltrim(const string &str) {
     string s(str);
     s.erase(
-        s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
     );
     return s;
 }
 
-string rtrim(const string &str)
-{
+string rtrim(const string &str) {
     string s(str);
     s.erase(
-        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
     );
     return s;
 }
 
 vector<string> split(const string &str) {
     vector<string> tokens;
-
     string::size_type start = 0;
-    string::size_type end = 0;
+    string::size_type end   = 0;
 
     while ((end = str.find(" ", start)) != string::npos) {
         tokens.push_back(str.substr(start, end - start));
-
         start = end + 1;
     }
 
     tokens.push_back(str.substr(start));
-
     return tokens;
 }

55583bec447ce48f222934d414e41209a46fb5ce
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-11-30 10:34:21 +0900

    Update Maximum-Palindromes.java

--- Maximum-Palindromes.java
+++ Maximum-Palindromes.java
@@ -9,3 +9,12 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
+
+    static final int MOD = 1000000007;
+    static final int MAXN = 200000;
+
+    static long[] fact = new long[MAXN + 1];
+    static long[] invfact = new long[MAXN + 1];
+    static int[][] prefixCnt = new int[26][MAXN + 1];
+    static String S;
